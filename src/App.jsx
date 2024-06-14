c0838849155aa3ee89a2b9a6c24f97648f03e840
NguyenNgan03 <simple.270123@gmail.com>
2024-06-14 16:55:52 +0700

    create Score

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,25 @@
-import pnLogo from "./assets/pn-logo.png";
+import React from "react";
 import Header from "./components/Header";
+import Scores from "./components/Scores";
+import {
+  JAVA_RESULTS,
+  PYTHON_RESULTS,
+  HTML_RESULTS,
+  ENGLISH_RESULTS,
+} from "./data";
+
+
 function App() {
   return (
-    <>
-      <Header>
-        
-     </Header>
-
-      <main className="scores-container">
-        <div class="scores">
-          <h1>Fake Course</h1>
-
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </main>
-    </>
+    <main className="app-container">
+      <Header />
+      <div className="scores-wrapper">
+        <Scores courseName="Java" courseResults={JAVA_RESULTS} />
+        <Scores courseName="HTML" courseResults={HTML_RESULTS} />
+        <Scores courseName="Python" courseResults={PYTHON_RESULTS} />
+        <Scores courseName="English" courseResults={ENGLISH_RESULTS} />
+      </div>
+    </main>
   );
 }
 
